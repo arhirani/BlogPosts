946d3ed827a22639e72cec2124d5dfebb60caa63
Rahul Hirani <arhirani56@gmail.com>
2023-02-17 00:08:29 +0530

    sample changes

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,56 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Table } from "reactstrap";
 
 const DisplayPosts = ({ post, index }) => {
-    const url = `/blogs/${post.userId}/${post.id}`;
+  const url = `/blogs/${post.userId}/${post.id}`;
 
-    return (
-        <tr>
-            <th>{index + 1}</th>
-            <th><Link to={url}>{post.title}</Link></th>
-        </tr>
-    )
-}
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <th>
+        <Link to={url}>{post.title}</Link>
+      </th>
+    </tr>
+  );
+};
 
 const Blogs = ({ userId }) => {
+  const URL = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+  const [post, setPosts] = useState([]);
 
-    const URL = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
-    const [post, setPosts] = useState([]);
-
-    useEffect(() => {
-        if (!post.length) {
-            fetchPosts(userId);
-        }
-    });
-
-    let fetchPosts = async () => {
-        const post = await fetch(URL).then(res => res.json());
-        setPosts(post);
+  useEffect(() => {
+    if (!post.length) {
+      fetchPosts(userId);
     }
+  });
 
-    return (
-        <Table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                </tr>
-            </thead>
-            {post.map((p, i) => <DisplayPosts post={p} index={i} />)}
-        </Table>
-    )
-}
+  let fetchPosts = async () => {
+    const post = await fetch(URL).then((res) => res.json());
+    setPosts(post);
+  };
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title (Heading)</th>
+        </tr>
+      </thead>
+      {post.map((p, i) => (
+        <DisplayPosts post={p} index={i} />
+      ))}
+    </Table>
+  );
+};
 
 const DisplayBlogs = (props) => {
+  const id = parseInt(window.location.href.split("/")[5]);
 
-    const id = parseInt(window.location.href.split('/')[5]);
-
-    return (
-        <>
-            {<Blogs userId={id} />}
-        </>
-    )
-}
+  return <>{<Blogs userId={id} />}</>;
+};
 
 export default DisplayBlogs;
